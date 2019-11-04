c3f364c241c7d44bb2bbb3f0990970b29394245d
Mudreco <nvo_joel_msn@hotmail.com>
2019-11-04 17:24:42 -0300

    Game Screen ceation and progress

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from "./components/Header";
 import StartScreen from './components/screens/StartScreen'
+import GameScreen from './components/screens/GameScreen'
+
 export default function App() {
+  const [useNumber, setUseNumber] = useState();
+  const startGameHandler = (selectedNumber) => {
+    setUseNumber(selectedNumber);
+  }
+
+  let content = <StartScreen onStartGame={startGameHandler}/>;
+  if(useNumber){
+    content = <GameScreen userChoice={useNumber}/>
+  }
+
   return (
     <View style={styles.container}>
       <Header title="Test"/>
-      <StartScreen/>
+      {content}
     </View>
   );
 }
--- components/screens/StartScreen.js
+++ components/screens/StartScreen.js
@@ -30,7 +30,7 @@ const StartScreen = props => {
     if (confirmed){
         confirmedOutput = <Card style={styles.summaryContainer}><Text>You selected</Text>
         <NumberContainer>{selectedNumber}</NumberContainer>
-        <Button title='START GAME'/>
+        <Button title='START GAME' onPress={() => props.onStartGame(selectedNumber)}/>
         </Card>
     }
 
